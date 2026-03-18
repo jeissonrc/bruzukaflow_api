72d332a3b605f6ee2530247a1946d5b6b904adbc
Jeisson R. Cunha <jeissonrc@gmail.com>
2026-03-18 13:39:56 -0300

    Adicionado o database.js

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('brazucaflow_bd', 'root', '', {
+  host: '127.0.0.1',
+  dialect: 'mysql',
+  logging: false
+});
+
+module.exports = sequelize;
+
+
+
